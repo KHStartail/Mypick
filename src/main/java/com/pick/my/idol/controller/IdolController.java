0a513c33a9587b33d95a7c177a246dcddab6b425
sim <ttt99619@gmail.com>
2021-11-08 16:23:00 +0900

    don't download

--- src/main/java/com/pick/my/idol/controller/IdolController.java
+++ src/main/java/com/pick/my/idol/controller/IdolController.java
@@ -2,6 +2,8 @@ package com.pick.my.idol.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pick.my.idol.service.IdolService;
 
@@ -10,5 +12,11 @@ public class IdolController {
 	
 	@Autowired
 	private IdolService service;
+	
+	@RequestMapping(value="idolRegister.pi", method=RequestMethod.POST)
+	public String registerIdol (){
+		
+		return "";
+	}
 	
 }
